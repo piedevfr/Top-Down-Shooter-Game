fc024a81caa29d20647287528b45e31e757a0575
PieDev <harmony.nkweini@gmail.com>
2026-04-05 23:22:22 +0100

    nothing much
    >added instructions
    >changed bullet speed

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,7 +1,7 @@
 class_name Bullet
 extends Node2D
 
-const SPEED = 400
+const SPEED = 800
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
