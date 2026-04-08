e67b7e1666492d23193e4a6d4e7bf0b506cd257d
Dr-Sauss <ayomideoyelahan@gmail.com>
2026-04-08 22:18:24 +0100

    fixed some stuff
    gemini helped me fix some stuff, by opening the Entity.tscn in VS Code and replacing some stuff.
    heres the gemini chat cuz i didn't read everything:
    
    https://gemini.google.com/share/a16a2f516de5

--- Scripts/Classes/Class_Entity.gd
+++ Scripts/Classes/Class_Entity.gd
@@ -9,4 +9,5 @@ var Health : int = 100
 
 func _on_hurtbox_collider_body_entered(body: Node2D) -> void:
 	if body is Bullet:
-		Health -= Global.BULLET_DAMAGE
+		#Health -= Global.BULLET_DAMAGE
+		pass
