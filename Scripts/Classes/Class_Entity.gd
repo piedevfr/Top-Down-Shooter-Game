3f33433bab14f8d6b4a01d80b3b75da23b78ee75
PieDev <harmony.nkweini@gmail.com>
2026-04-10 14:24:57 +0100

    Revert "fixed the conflicts"
    This reverts commit 8da97fd3c7aa52fc03d629d63f954a15728b32de.

--- Scripts/Classes/Class_Entity.gd
+++ Scripts/Classes/Class_Entity.gd
@@ -1,5 +1,13 @@
+extends CharacterBody2D
+
+class_name Entity
+
+@onready var hitbox: CollisionShape2D = $Hitbox
+@onready var hurtbox_collider: Area2D = $HurtboxCollider
+
 var Health : int = 100
 
 func _on_hurtbox_collider_body_entered(body: Node2D) -> void:
-    if body is Bullet:
-        Health -= Global.BULLET_DAMAGE
+	if body is Bullet:
+		#Health -= Global.BULLET_DAMAGE
+		pass
