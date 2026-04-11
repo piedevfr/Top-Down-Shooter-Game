f7e87114fc035cf05e8bf4809827caba54624ab0
Dr-Sauss <ayomideoyelahan@gmail.com>
2026-04-11 13:20:17 +0100

    small changes

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,6 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 @onready var ammo: Label = $Ammo
+@export var show_health : bool
 
 var speed = 150.0
 @export var gun : Gun
@@ -14,6 +15,10 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
-	ammo.text = "Ammo: %s" % gun.ammo
+	if show_health == true:
+		ammo.visible = true
+		ammo.text = "Ammo: %s" % gun.ammo
+	elif show_health == false:
+		ammo.visible = false
 	
 	move_and_slide()
