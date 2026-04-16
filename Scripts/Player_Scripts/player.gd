34b9ff6c3091dd53bac9f9c3d714a8ff521913ee
PieDev <harmony.nkweini@gmail.com>
2026-04-16 22:25:59 +0100

    camera revamp

--- Scripts/Player_Scripts/player.gd
+++ Scripts/Player_Scripts/player.gd
@@ -1,11 +1,11 @@
 class_name Player
 extends CharacterBody2D
 
-#@onready var ammo: Label = $Ammo
+@onready var ammo: Label = $Ammo
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var show_ammo : bool
-@export var gun : Gun
+@export var gun : Node2D
 
 var speed = 150.0
 
@@ -17,11 +17,11 @@ func _physics_process(_delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
-	#if show_ammo == true:
-	#	ammo.visible = true
-	#	ammo.text = "Ammo: %s" % gun.ammo
-	#elif show_ammo == false:
-	#	ammo.visible = false
+	if show_ammo == true:
+		ammo.visible = true
+		ammo.text = "Ammo: %s" % gun.ammo
+	elif show_ammo == false:
+		ammo.visible = false
 	rotate_sprite()
 	
 	move_and_slide()
--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -2,5 +2,23 @@ extends Camera2D
 
 @onready var player: Node2D = $".."
 
+const MAXDISTANCE = 48
+
+var target_distance = 0
+var center_pos = position
+
 func _process(delta: float) -> void:
-	position = player.position.lerp(get_global_mouse_position(), 1.0/12.0)
+	var direction = center_pos.direction_to(get_local_mouse_position())
+	var target_pos = center_pos + direction * target_distance
+	
+	target_pos = target_pos.clamp(
+		center_pos - Vector2(MAXDISTANCE, MAXDISTANCE),
+		center_pos + Vector2(MAXDISTANCE, MAXDISTANCE)
+	)
+	
+	position = target_pos
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		target_distance = center_pos.distance_to(get_local_mouse_position()) / 1.25
+		
